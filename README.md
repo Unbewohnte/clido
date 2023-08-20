0e633c2390b9dd53df6ad33a8d5481bad5a40466
Unbewohnte <me@unbewohnte.su>
2023-08-20 22:32:39 +0300

    feat: version command; fix: moved from MIT to GPLv3 license; docs: updated README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # clido
 ## Command Line Interface to DO program
 
-clido helps to keep "to do" notes on the disk space.
+clido helps to track and keep "to do" notes on the disk.
 
 
 ## Compilation
@@ -11,7 +11,7 @@ Simply `make` or as simply compile .c files together
 
 ## Installation
 
-If you're on Linux - `make install` or simply move the binary to ...TODO!!(figure out where to put the binary)
+If you're on Linux - `sudo make install` **or** `sudo ./install.sh` **or if not** simply move the binary to wherever you desire.
 
 
 ## Usage
@@ -20,12 +20,21 @@ If you're on Linux - `make install` or simply move the binary to ...TODO!!(figur
 
 Commands:
 
-- `help -> Prints this message and exists`
+- `help -> Prints help message and exists`
+- `version -> Prints version information and exits`
 - `add [todo]... -> Writes a new TODO to a default TODO file`
 - `show -> Outputs current TODOs`
 - `show-done -> Outputs TODOs which were done previously`
 - `done [index]... -> Marks specified TODO(s) as done`
+- `todo-path [path] -> Specifies another path pointing to the todo file`
 
+
+## Examples
+
+- `clido add Do the cooking today` -> appends a new todo with given text to a todo file
+- `clido show` -> prints current todos
+- `clido done 0 1` -> marks todos with indexes 0 and 1 as done
+- `clido show-done` -> prints todos which are done at this point
 
 ## License
 ![GPLv3](https://www.gnu.org/graphics/gplv3-with-text-84x42.png "GPLv3 logo")
--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
 /*
- The MIT License (MIT)
+    clido - Command Line Interface to DO program
+    Copyright (C) 2023 Kasyanov Nikolay Alexeyevich (Unbewohnte)
 
-Copyright © 2023 Kasyanov Nikolay Alexeyevich (Unbewohnte)
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
@@ -20,13 +26,17 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 #include "todo.h"
 
 
+#define CLIDO_VERSION "0.1"
+
+
 #ifdef _WIN32
-#define TODO_FILE_PATH "./TODOS.bin"
+#define TODO_FILE_PATH "TODOS.bin"
 
 #else
 #define TODO_FILE_PATH "/usr/local/share/TODOS.bin"
 
 #endif
+
 
 /*
 Closes a todo file and frees up memory allocated to store todos, basically
@@ -111,6 +121,7 @@ int main(int argc, char** argv) {
                 "clido [COMMAND]\
                 \n\n[COMMAND]:\
                 \nhelp -> Prints this message and exists\
+                \nversion -> Prints version information and exits\
                 \nadd [todo] -> Writes a new TODO to a default TODO file\
                 \nshow -> Outputs current TODOs\
                 \nshow-done -> Outputs TODOs which were done previously\
@@ -127,6 +138,19 @@ int main(int argc, char** argv) {
             clean_up(todo_file, todos, todo_count);
             return EXIT_SUCCESS;
         }
+
+
+        // Version command
+        if (strcmp(arg, "version") == 0) {
+            printf(
+                "clido v%s- Command Line Interface to DO program\
+                \n(c) 2023 Kasyanov Nikolay Alexeyevich (Unbewohnte)\
+                \n", CLIDO_VERSION
+            );
+            clean_up(todo_file, todos, todo_count);
+            return EXIT_SUCCESS;
+        }
+
 
         // Todo-path command
         if (strcmp(arg, "todo-path") == 0) {
